009e3f86f36670bda2ce0f2949cb6d22d64cd721
Aryan Kale <aryankale22@gmail.com>
2023-04-16 19:53:35 -0400

    Added post and delete routes for students

--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -2,45 +2,13 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
 students = Blueprint('students', __name__)
-
 
 # Get all of the information for a student
 @students.route('/<ID>', methods=['GET'])
 def get_student_info(ID):
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM Students WHERE StudentID = {0}'.format(ID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# Get favorites colleges for a specific student
-@students.route('/<ID>/favoritedColleges', methods=['GET'])
-def get_student_favorite_colleges(ID):
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT CollegeId FROM StudentsFavoritedColleges WHERE StudentID = {0}'.format(ID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-# Get the corresponding student's college preferences
-@students.route('/<ID>/collegePreferences', methods=['GET'])
-def get_student_college_preferences(ID):
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT GreekLife, Size, Temperature FROM CollegePreferences WHERE StudentID = {0}'.format(ID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -66,12 +34,29 @@ def get_student_parent_income(ID):
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get the corresponding student's college preferences
+@students.route('/<ID>/collegePreferences', methods=['GET'])
+def get_student_college_preferences(ID):
+    cursor = db.get_db().cursor()
+
+    cursor.execute('SELECT GreekLife, Size, Temperature FROM CollegePreferences WHERE StudentID = {0}'.format(ID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+
 # Update the students college preferences
 @students.route('/<ID>/updatePreferences', methods=['PUT'])
 def update_student_preferences(ID):
     the_data = request.json
 
-    # these are the things of what you called them in appsmith
     greek_life = the_data['greek_life']
     size = the_data['size']
     temp = the_data['temp']
@@ -85,4 +70,54 @@ def update_student_preferences(ID):
     db.get_db().commit()
 
     return "Successfully updated student preferences!"
+
+
+# Get favorites colleges for a specific student
+@students.route('/<ID>/favoritedColleges', methods=['GET'])
+def get_student_favorite_colleges(ID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT CollegeId FROM StudentsFavoritedColleges WHERE StudentID = {0}'.format(ID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Add a college to a student's favorited colleged
+@students.route('/<ID>/favoritedColleges', methods=['POST'])
+def add_favorite_colleges(ID):
+    the_data = request.json
+
+    college_id = the_data['college_id']
+
+    the_query = 'INSERT into StudentsFavoritedColleges (StudentID, CollegeID) VALUES (%s, %s);'
+
+    current_app.logger.info(the_query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(the_query, (ID, college_id))
+    db.get_db().commit()
+
+    return "Successfully added a college to a student's favorited list!"
+
+# Delete a college from a student's favorited colleged
+@students.route('/<ID>/favoritedColleges', methods=['DELETE'])
+def delete_favorite_colleges(ID):
+    the_data = request.json
+
+    college_id = the_data['college_id']
+
+    the_query = 'DELETE FROM StudentsFavoritedColleges WHERE StudentID = %s AND CollegeID = %s;'
+
+    current_app.logger.info(the_query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(the_query, (ID, college_id))
+    db.get_db().commit()
+
+    return "Successfully deleted a college from a student's favorited list!"
 

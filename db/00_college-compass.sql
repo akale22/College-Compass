4648362755358035784f309a940cede132181fb5
Aryan Kale <aryankale22@gmail.com>
2023-04-17 12:30:15 -0400

    Fixed data

--- db/00_college-compass.sql
+++ db/00_college-compass.sql
@@ -36,7 +36,7 @@ CREATE TABLE Colleges
 DROP TABLE IF EXISTS Departments;
 CREATE TABLE Departments
 (
-    DeptName VARCHAR(10) NOT NULL,
+    DeptName VARCHAR(20) NOT NULL,
     DeptCode INT NOT NULL,
     CollegeID INT NOT NULL,
     DeptRank INT NOT NULL,

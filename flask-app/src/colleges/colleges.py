6e6996849b40a46caa39faf3899a338f82c4807a
mocartv <102615042+rastogi-ay@users.noreply.github.com>
2023-04-17 12:26:08 -0400

    added a students favorited colleges route

--- flask-app/src/colleges/colleges.py
+++ flask-app/src/colleges/colleges.py
@@ -2,13 +2,11 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
 colleges = Blueprint('colleges', __name__)
 
 @colleges.route('/<ID>', methods=['GET'])
 def get_college_info(ID):
     cursor = db.get_db().cursor()
-
     cursor.execute('SELECT * FROM Colleges WHERE CollegeID = {0}'.format(ID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -25,8 +23,7 @@ def get_college_info(ID):
 @colleges.route('/<ID>/departments', methods=['GET'])
 def get_college_departments(ID):
     cursor = db.get_db().cursor()
-
-    cursor.execute('SELECT DeptRank, DeptDescription  FROM Colleges JOIN Departments WHERE Colleges.CollegeID = {0} ORDER BY DeptRank'.format(ID))
+    cursor.execute('SELECT DeptRank, DeptDescription FROM Colleges JOIN Departments WHERE Colleges.CollegeID = {0} ORDER BY DeptRank'.format(ID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -40,9 +37,8 @@ def get_college_departments(ID):
 
 @colleges.route('/<ID>/courses', methods=['GET'])
 def get_college_courses(ID):
-    cursor = db.get_db().cursor()
-
-    #get college departments, have to join on colleges
+    cursor = db.get_db().cursor() 
+    # get college departments, have to join on colleges
     cursor.execute('SELECT CourseName, CourseDescription, Credits FROM Colleges JOIN Departments D JOIN Courses on D.DeptCode = Courses.DeptCode WHERE Colleges.CollegeID = {0}'.format(ID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -55,16 +51,16 @@ def get_college_courses(ID):
     the_response.mimetype = 'application/json'
     return the_response
 
-#get all colleges with query params:
+# get all colleges with query params:
 @colleges.route('/', methods=['GET'])
 def get_all_colleges():
     cursor = db.get_db().cursor()
-    #been messing around with this and the only real thing you can filter by in a college is the enrollmentSize, so you might
-    #as well just pass it through the url instead of query paramters tho idk
+    # been messing around with this and the only real thing you can filter by in a college is the enrollmentSize, so you might
+    # as well just pass it through the url instead of query paramters tho idk
 
-    #default value if the user does not supply a paramter
+    # default value if the user does not supply a paramter
     enrollmentSize = request.args.get('enrollmentSize', 10000000)
-    #enrollmentSize = request.args.to_dict()['enrollmentSize']
+    # enrollmentSize = request.args.to_dict()['enrollmentSize']
     the_query ='SELECT * FROM Colleges WHERE EnrollmentSize < ' + str(enrollmentSize)
 
     cursor.execute(the_query)
@@ -111,6 +107,21 @@ def add_new_college(ID):
     db.get_db().commit()
 
     return "Successfully added a course to a college!"
+
+@colleges.route('/<ID>/studentsInterested', methods=['GET'])
+def get_interested_students(ID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT studentID FROM StudentsFavoritedColleges WHERE CollegeID = {0}'.format(ID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 # Add a new department to a college
 
